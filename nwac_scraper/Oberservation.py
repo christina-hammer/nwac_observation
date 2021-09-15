46b235a42eda36fa3fff1d1bbc9dcabbe1414b06
Kit Hammer <me@kithammer.com>
2021-09-15 16:53:41 -0700

    printing td text, added observation class

---
+++ nwac_scraper/Oberservation.py
@@ -0,0 +1,14 @@
+#Observation class
+
+class Observation:
+
+	def __init__(self):
+		self.Date = ""
+		self.ObserverType = ""
+		self.Region = ""
+		self.Latitude = ""
+		self.Longitude = ""
+		self.LocationName = ""
+		self.Avalanche = false
+		self.SignsOfInstability = false
+		self.Url = ""
--- nwac_scraper/main.py
+++ nwac_scraper/main.py
@@ -1,11 +1,30 @@
 ##nwac observation scraper
 import requests
 import bs4
+from geopy.geocoders import Nominatim
 
-url = "https://nwac.us/observations/?season=2021&search="
-page = requests.get(url)
 
-soup = bs4.BeautifulSoup(page.content, 'lxml')
-table = soup.find(name='table', attrs={'id':'observations'})
+def main():
+	url = "https://nwac.us/observations/?season=2021&search="
+	page = requests.get(url)
 
-print(table)
+	soup = bs4.BeautifulSoup(page.content, 'lxml')
+	table = soup.find(name='table', attrs={'id':'observations'})
+
+	rows = table.tbody.findAll('tr')
+
+	geolocator = Nominatim(user_agent="nwac_observations")
+	##will only search for locations in WA or Oregon
+	##all oregon observations will be in Mt. Hood zone
+	geocodeWA = lambda query: geolocator.geocode("%s WA" % query)
+	geocodeOR = lambda query: geolocator.geocode("%s OR" % query)
+
+
+	for i in range(0, 4):
+		for td in rows[i].findAll('td'):
+			print(td.text)
+
+if __name__ == "main":
+	main()
+
+
