ba34b5ff5bb3e1e6a8fb05f23079e8b91df53fc8
Kit Hammer <me@kithammer.com>
2021-09-15 15:35:44 -0700

    reading obs table from first page

--- nwac_scraper/main.py
+++ nwac_scraper/main.py
@@ -1,3 +1,11 @@
-##main.py
 ##nwac observation scraper
-print("Hello World!")
+import requests
+import bs4
+
+url = "https://nwac.us/observations/?season=2021&search="
+page = requests.get(url)
+
+soup = bs4.BeautifulSoup(page.content, 'lxml')
+table = soup.find(name='table', attrs={'id':'observations'})
+
+print(table)
