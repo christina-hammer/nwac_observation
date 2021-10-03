9efbbfbf3d15e27c13a415d55171a9036c2d7b31
Kit Hammer <me@kithammer.com>
2021-10-03 16:13:45 -0700

    add react app

---
+++ nwac-observations/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="page-header">
+      <h1>NWAC Observation Map</h1>
+        <p style={{ color:'pink'}}>
+          This is where a disclaimer belongs.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
